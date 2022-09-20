5bc30ba31da2615018b2c37443524bd5915df742
williamtyn <william@tyner.se>
2022-09-20 16:47:07 +0000

    add basic structure of functions i js

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
